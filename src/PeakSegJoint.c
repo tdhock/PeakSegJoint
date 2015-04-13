98a49e26fa6821b5a698528603fa8699d3521828
Toby Dylan Hocking <tdhock5@gmail.com>
2015-04-13 18:19:14 -0400

    bug in mean computation

--- src/PeakSegJoint.c
+++ src/PeakSegJoint.c
@@ -436,24 +436,39 @@ PeakSegJointHeuristicStep2
 	  right_cumsum_vec[bin_i] = right_cumsum_value;
 	}
       }//for(diff_i
+      printf("left bases_per_bin=%d n_peaks=%d\n", bases_per_bin, n_peaks);
+      for(diff_i=0;diff_i<n_peaks;diff_i++){
+      	sample_i = model->samples_with_peaks_vec[diff_i];
+      	left_cumsum_vec = left_cumsum_mat + n_bins*sample_i;
+      	for(bin_i=0;bin_i<n_bins;bin_i++){
+      	  printf("%d ", left_cumsum_vec[bin_i]);
+      	}
+      	printf("\n");
+      }
+      printf("\n");
       /* 
 	 cumsum matrices have been computed, so now use them to
 	 compute the loss and feasibility of all models.
        */
       for(seg1_LastIndex=0; seg1_LastIndex < n_bins; seg1_LastIndex++){
 	peakStart = left_chromStart + (seg1_LastIndex+1)*bases_per_bin;
+	printf("[%d] seg1 cumsum ", seg1_LastIndex);
 	for(diff_i=0; diff_i < n_peaks; diff_i++){
 	  sample_i = model->samples_with_peaks_vec[diff_i];
 	  left_cumsum_vec = left_cumsum_mat + n_bins*sample_i;
 	  cumsum_value = left_cumsum_vec[seg1_LastIndex];
+	  printf("%d ", cumsum_value);
 	  bases_value = peakStart - model_list->seg_start_end[0];
 	  mean_value = cumsum_value/bases_value;
 	  seg1_mean_vec[sample_i] = mean_value;
 	  loss_value = OptimalPoissonLoss(cumsum_value, mean_value);
 	  seg1_loss_vec[sample_i] = loss_value;
 	}
+	printf("\n");
 	for(seg2_LastIndex=0; seg2_LastIndex < n_bins; seg2_LastIndex++){
 	  peakEnd = right_chromStart + (seg2_LastIndex+1)*bases_per_bin;
+	  printf("\npeaks=%d[%d,%d]bases_per_bin=%d\n", 
+		 n_peaks, peakStart, peakEnd, bases_per_bin);
 	  total_loss = model_list->model_vec[0].loss[0];
 	  if(peakEnd <= peakStart){
 	    total_loss = INFINITY;
@@ -482,14 +497,20 @@ PeakSegJointHeuristicStep2
 	    seg3_mean_vec[sample_i] = mean_value;
 	    loss_value = OptimalPoissonLoss(cumsum_value, mean_value);
 	    total_loss += loss_value;
+	    printf("[%d] %f %f %f\n",
+	    	   sample_i,
+	    	   seg1_mean_vec[sample_i],
+	    	   seg2_mean_vec[sample_i],
+	    	   seg3_mean_vec[sample_i]);
 	    //if not feasible, loss is infinite.
 	    if(seg2_mean_vec[sample_i] <= seg1_mean_vec[sample_i] ||
 	       seg2_mean_vec[sample_i] <= seg3_mean_vec[sample_i]){
-	      loss_value = INFINITY;
+	      total_loss = INFINITY;
 	    }
 	  }	  
-	  if(loss_value < model->loss[0]){
-	    model->loss[0] = loss_value;
+	  printf("loss=%f\n", total_loss);
+	  if(total_loss < model->loss[0]){
+	    model->loss[0] = total_loss;
 	    model->peak_start_end[0] = peakStart;
 	    model->peak_start_end[1] = peakEnd;
 	    for(diff_i=0; diff_i < n_peaks; diff_i++){
