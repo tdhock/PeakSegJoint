f88329a769d99fe032ae9ff379e53abac406c771
Toby Dylan Hocking <tdhock5@gmail.com>
2015-05-07 14:35:09 -0400

    loss difference feature between 1 peak and 0 peak models

--- R/features.R
+++ R/features.R
@@ -1,5 +1,7 @@
 ### Compute the feature matrix for this joint segmentation problem.
-featureMatrix <- function(profile.list){
+featureMatrix <- structure(function(profile.list){
+  stopifnot(is.list(profile.list))
+  stopifnot(is.data.frame(profile.list[[1]]))
   features.by.sample <- list()
   min.chromEnd <- min(sapply(profile.list, with, chromEnd[length(chromEnd)]))
   max.chromStart <- max(sapply(profile.list, with, chromStart[1]))
@@ -7,6 +9,12 @@ featureMatrix <- function(profile.list){
   for(sample.id in names(profile.list)){
     ## Compute feature vector for learning using this segmentation
     ## problem.
+    one.list <- profile.list[sample.id]
+    fit <- PeakSegJointHeuristic(one.list)
+    loss <- sapply(fit$models, "[[", "loss")
+    if(length(loss) == 1){
+      loss <- c(loss, loss)
+    }
     sample.counts <- profile.list[[sample.id]]
     too.long <- with(sample.counts, rep(count, chromEnd-chromStart))
     too.long.pos <- with(sample.counts, {
@@ -22,7 +30,8 @@ featureMatrix <- function(profile.list){
         sd=sd(long),
         mad=mad(long),
         bases=bases,
-        sum=sum(long))
+        sum=sum(long),
+        diff.loss=loss[1]-loss[2])
     suppressWarnings({
       features.by.sample[[sample.id]] <-
         c(feature.vec,
@@ -33,4 +42,13 @@ featureMatrix <- function(profile.list){
   }
   do.call(rbind, features.by.sample)
 ### Numeric feature matrix (samples x features).
-}
+}, ex=function(){
+  library(PeakSegJoint)
+  data(H3K36me3.TDH.other.chunk1)
+  lims <- c(43000000, 43200000) # left
+  some.counts <-
+    subset(H3K36me3.TDH.other.chunk1$counts,
+           lims[1] < chromEnd & chromStart < lims[2])
+  profile.list <- ProfileList(some.counts)
+  featureMatrix(profile.list)
+})
