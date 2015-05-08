d3d821286cba0a413638f9a6e74b597076669734
Toby Dylan Hocking <tdhock5@gmail.com>
2015-05-08 15:16:07 -0400

    PeakErrorSamples

---
+++ R/PeakErrorSamples.R
@@ -0,0 +1,22 @@
+### Compute PeakError for several samples.
+PeakErrorSamples <- function(peaks, regions){
+  stopifnot(is.data.frame(peaks))
+  stopifnot(is.data.frame(regions))
+  regions.by.sample <- split(regions, regions$sample.id, drop=TRUE)
+  peaks.by.sample <- split(peaks, peaks$sample.id, drop=TRUE)
+  error.by.sample <- list()
+  for(sample.id in names(regions.by.sample)){
+    sample.regions <- regions.by.sample[[sample.id]]
+    sample.peaks <- if(sample.id %in% names(peaks.by.sample)){
+      peaks.by.sample[[sample.id]]
+    }else{
+      Peaks()
+    }
+    sample.error <- PeakErrorChrom(sample.peaks, sample.regions)
+    error.by.sample[[sample.id]] <-
+      data.frame(sample.id, sample.error)
+  }
+  err <- do.call(rbind, error.by.sample)
+  rownames(err) <- NULL
+  err
+}
