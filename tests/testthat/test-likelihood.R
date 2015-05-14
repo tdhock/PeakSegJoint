8acd6e24fe6ff321b23b4d7fce7373b0fa1c120d
Toby Dylan Hocking <tdhock5@gmail.com>
2015-05-14 13:04:47 -0400

    remove plots

--- tests/testthat/test-likelihood.R
+++ tests/testthat/test-likelihood.R
@@ -1,7 +1,7 @@
+library(testthat)
 context("Poissson likelihood")
 
 library(PeakSegJoint)
-library(testthat)
 
 data(H3K36me3.TDH.other.chunk1)
 
@@ -11,131 +11,73 @@ some.counts <-
 profile.list <- split(some.counts, some.counts$sample.id)
 sample.peak.list <- list()
 
-param.values <- c(2, 3, 5)
-cDPA.seconds.list <- list()
-heuristic.seconds.list <- list()
-loss.list <- list()
-for(sample.i in seq_along(profile.list)){
-  sample.id <- names(profile.list)[[sample.i]]
-  cat(sprintf("%4d / %4d %s\n", sample.i, length(profile.list), sample.id))
-  sample.counts <- profile.list[[sample.id]]
-  m.args <- list()
-  result.list <- list()
-  for(param in param.values){
-    param.name <- paste(param)
-    param.int <- as.integer(param)
-    fit <- PeakSegJointHeuristic(sample.counts, param.int)
-    model <- fit$models[[2]]
-    peak <- model$peak_start_end
-    result.list[[param.name]] <- 
-      data.frame(param.int,
-                 chromStart=peak[1],
-                 chromEnd=peak[2],
-                 fit.loss=model$loss,
-                 fit.seg1=model$seg1_mean_vec,
-                 fit.seg2=model$seg2_mean_vec,
-                 fit.seg3=model$seg3_mean_vec)
-  }
-  results <- do.call(rbind, result.list)
-
-  count.vec <- with(sample.counts, rep(count, chromEnd-chromStart))
-  sample.bases <- sum(with(sample.counts, chromEnd-chromStart))
-  stopifnot(all.equal(sample.bases, length(count.vec)))
-  seg1.chromStart <- sample.counts$chromStart[1]
-  seg3.chromEnd <- sample.counts[nrow(sample.counts), "chromEnd"]
-  seg1.chromEnd <- results$chromStart
-  seg2.chromEnd <- results$chromEnd
-  seg1.first <- seg1.chromStart - seg1.chromStart + 1
-  seg1.last <- seg1.chromEnd - seg1.chromStart
-  seg2.first <- seg1.last + 1
-  seg2.last <- seg2.chromEnd - seg1.chromStart
-  seg3.first <- seg2.last + 1
-  seg3.last <- length(count.vec)
-  data.sizes <- rowSums(cbind(seg3.last-seg2.last,
-                              seg2.last-seg1.last,
-                              seg1.last))
-  stopifnot(data.sizes == length(count.vec))
-  results$expected.loss <- results$expected.seg1 <-
-    results$expected.seg2 <- results$expected.seg3 <- NA
-  for(model.i in 1:nrow(results)){
-    model <- results[model.i, ]
-    seg.indices <-
-      data.frame(first=c(seg1.first, seg2.first[model.i], seg3.first[model.i]),
-                 last=c(seg1.last[model.i], seg2.last[model.i], seg3.last))
-    seg.loss.list <- list()
-    for(seg.i in 1:nrow(seg.indices)){
-      col.name <- paste0("expected.seg", seg.i)
-      s <- seg.indices[seg.i, ]
-      seg.data <- count.vec[s$first:s$last]
-      seg.mean <- results[[col.name]][model.i] <- mean(seg.data)
-      seg.loss.list[[seg.i]] <- PoissonLoss(seg.data, seg.mean)
+test_that("C loss/segment means are correct", {
+  param.values <- c(2, 3, 5)
+  cDPA.seconds.list <- list()
+  heuristic.seconds.list <- list()
+  loss.list <- list()
+  for(sample.i in seq_along(profile.list)){
+    sample.id <- names(profile.list)[[sample.i]]
+    cat(sprintf("%4d / %4d %s\n", sample.i, length(profile.list), sample.id))
+    sample.counts <- profile.list[[sample.id]]
+    m.args <- list()
+    result.list <- list()
+    for(param in param.values){
+      param.name <- paste(param)
+      param.int <- as.integer(param)
+      fit <- PeakSegJointHeuristic(sample.counts, param.int)
+      model <- fit$models[[2]]
+      peak <- model$peak_start_end
+      result.list[[param.name]] <- 
+        data.frame(param.int,
+                   chromStart=peak[1],
+                   chromEnd=peak[2],
+                   fit.loss=model$loss,
+                   fit.seg1=model$seg1_mean_vec,
+                   fit.seg2=model$seg2_mean_vec,
+                   fit.seg3=model$seg3_mean_vec)
     }
-    results$expected.loss[model.i] <- sum(unlist(seg.loss.list))
+    results <- do.call(rbind, result.list)
+
+    count.vec <- with(sample.counts, rep(count, chromEnd-chromStart))
+    sample.bases <- sum(with(sample.counts, chromEnd-chromStart))
+    stopifnot(all.equal(sample.bases, length(count.vec)))
+    seg1.chromStart <- sample.counts$chromStart[1]
+    seg3.chromEnd <- sample.counts[nrow(sample.counts), "chromEnd"]
+    seg1.chromEnd <- results$chromStart
+    seg2.chromEnd <- results$chromEnd
+    seg1.first <- seg1.chromStart - seg1.chromStart + 1
+    seg1.last <- seg1.chromEnd - seg1.chromStart
+    seg2.first <- seg1.last + 1
+    seg2.last <- seg2.chromEnd - seg1.chromStart
+    seg3.first <- seg2.last + 1
+    seg3.last <- length(count.vec)
+    data.sizes <- rowSums(cbind(seg3.last-seg2.last,
+                                seg2.last-seg1.last,
+                                seg1.last))
+    stopifnot(data.sizes == length(count.vec))
+    results$expected.loss <- results$expected.seg1 <-
+      results$expected.seg2 <- results$expected.seg3 <- NA
+    for(model.i in 1:nrow(results)){
+      model <- results[model.i, ]
+      seg.indices <-
+        data.frame(first=c(seg1.first, seg2.first[model.i], seg3.first[model.i]),
+                   last=c(seg1.last[model.i], seg2.last[model.i], seg3.last))
+      seg.loss.list <- list()
+      for(seg.i in 1:nrow(seg.indices)){
+        col.name <- paste0("expected.seg", seg.i)
+        s <- seg.indices[seg.i, ]
+        seg.data <- count.vec[s$first:s$last]
+        seg.mean <- results[[col.name]][model.i] <- mean(seg.data)
+        seg.loss.list[[seg.i]] <- PoissonLoss(seg.data, seg.mean)
+      }
+      results$expected.loss[model.i] <- sum(unlist(seg.loss.list))
+    }
+    with(results, {
+      expect_equal(fit.loss, expected.loss)
+      expect_equal(fit.seg1, expected.seg1)
+      expect_equal(fit.seg2, expected.seg2)
+      expect_equal(fit.seg3, expected.seg3)
+    })
   }
-  with(results, {
-    expect_equal(fit.loss, expected.loss)
-    expect_equal(fit.seg1, expected.seg1)
-    expect_equal(fit.seg2, expected.seg2)
-    expect_equal(fit.seg3, expected.seg3)
-  })
-  results$status <- with(results, {
-    ifelse(fit.loss == min(fit.loss), "best", "not")
-  })
-  best.peak <- subset(results, fit.loss == min(fit.loss))[1, ]
-  results$chromStart.diff <- results$chromStart - best.peak$chromStart
-  results$chromEnd.diff <- results$chromEnd - best.peak$chromEnd
-
-  loss.list[[sample.id]] <- data.frame(sample.id, results)
-  
-  sample.peak.list[[sample.id]] <-
-    data.frame(sample.id,
-               y=-0.1 * (1:nrow(results)) * max(sample.counts$count),
-               results)
-}
-sample.peaks <- do.call(rbind, sample.peak.list)
-
-ggplot()+
-  geom_step(aes(chromStart/1e3, count),
-            data=some.counts,
-            color="grey50")+
-  geom_segment(aes(chromStart/1e3, y,
-                   color=factor(param),
-                   xend=chromEnd/1e3, yend=y),
-               data=sample.peaks,
-               size=2)+
-  theme_bw()+
-  theme(panel.margin=grid::unit(0, "cm"))+
-  facet_grid(sample.id ~ ., scales="free", labeller=function(var, val){
-    sub("McGill0", "", val)
-  })
-
-loss <- do.call(rbind, loss.list)
-loss$param.fac <- factor(loss$param)
-
-ggplot()+
-  theme_bw()+
-  theme(panel.margin=grid::unit(0, "cm"))+
-  facet_grid(sample.id + what ~ ., labeller=function(var, val){
-    sub("McGill0", "", val)
-  }, scales="free")+
-  xlab("bin factor suboptimality parameter")+
-  scale_size_manual(values=c(best=3, not=1))+
-  geom_point(aes(param.fac, fit.loss, size=status),
-             data=data.frame(fit.loss, what="loss"))+
-  geom_segment(aes(param.fac, chromStart,
-                   xend=param.fac, yend=chromEnd,
-                   size=status),
-               color="grey",
-               data=data.frame(loss, what="peak"))+
-  geom_text(aes(param.fac, chromStart,
-                label=chromStart.diff),
-            vjust=0,
-            size=3,
-            data=data.frame(loss, what="peak"))+
-  geom_text(aes(param.fac, chromEnd,
-                label=chromEnd.diff),
-            vjust=1,
-            size=3,
-            data=data.frame(loss, what="peak"))+
-  ggtitle("distance to best peak in bases")
-
+})
