2dd6438d2c2b7b773b3a86bfa7b21e08bbac8bda
Toby Dylan Hocking <tdhock5@gmail.com>
2015-07-20 15:14:24 -0400

    only write RData file if there are any predicted peaks

--- exec/Step3-segment-one-chrom.R
+++ exec/Step3-segment-one-chrom.R
@@ -54,93 +54,102 @@ step1.results.list <-
   mclapply.or.stop(seq_along(chrom.problems$problem.name), Step1Problem)
 
 if(all(sapply(step1.results.list, is.null))){
-  print(step1.problems.dt)
-  stop("no computable models for any uniform size segmentation problems")
-}
-step1.results <- do.call(rbind, step1.results.list)
+  print(chrom.problems)
+  warning("no computable models for any uniform size segmentation problems")
+}else{
+  step1.results <- do.call(rbind, step1.results.list)
 
-step2.problems <- clusterProblems(step1.results)
+  step2.problems <- clusterProblems(step1.results)
 
-## ggplot()+
-##   geom_segment(aes(problemStart, seq_along(problemStart),
-##                    xend=problemEnd, yend=seq_along(problemStart)),
-##                data=step1.results)+
-##   geom_segment(aes(problemStart, -problem.i,
-##                    xend=problemEnd, yend=-problem.i),
-##                data=step2.problems)
+  ## ggplot()+
+  ##   geom_segment(aes(problemStart, seq_along(problemStart),
+  ##                    xend=problemEnd, yend=seq_along(problemStart)),
+  ##                data=step1.results)+
+  ##   geom_segment(aes(problemStart, -problem.i,
+  ##                    xend=problemEnd, yend=-problem.i),
+  ##                data=step2.problems)
 
-SegmentStep2 <- function(row.i){
-  problem <- step2.problems[row.i, ]
-  profile.list <- readBigWigSamples(problem)
-  fit <- tryCatch({
-    PeakSegJointSeveral(profile.list)
-  }, error=function(e){
-    NULL
-  })
-  if(is.null(fit)){
-    return(NULL)
-  }
-  info <- ConvertModelList(fit)
-  if(is.data.frame(info$peaks)){
-    features <- featureMatrix(profile.list)
-    pred.log.lambda <- full.fit$predict(features)[[1]]
-    selected <- 
-      subset(info$modelSelection,
-             min.log.lambda < pred.log.lambda &
+  SegmentStep2 <- function(row.i){
+    problem <- step2.problems[row.i, ]
+    profile.list <- readBigWigSamples(problem)
+    fit <- tryCatch({
+      PeakSegJointSeveral(profile.list)
+    }, error=function(e){
+      NULL
+    })
+    if(is.null(fit)){
+      return(NULL)
+    }
+    info <- ConvertModelList(fit)
+    if(is.data.frame(info$peaks)){
+      features <- featureMatrix(profile.list)
+      pred.log.lambda <- full.fit$predict(features)[[1]]
+      selected <- 
+        subset(info$modelSelection,
+               min.log.lambda < pred.log.lambda &
                pred.log.lambda < max.log.lambda)
-    stopifnot(nrow(selected) == 1)
-    subset(info$peaks, peaks == selected$peaks)
+      stopifnot(nrow(selected) == 1)
+      subset(info$peaks, peaks == selected$peaks)
+    }
+  }
+  step2.peak.list <-
+    mclapply.or.stop(seq_along(step2.problems$problem.name), SegmentStep2)
+
+  pred.peaks <- do.call(rbind, step2.peak.list)
+
+  if(0 == nrow(pred.peaks)){
+    warning("no predicted peaks")
+  }else{
+
+    ## big.problem <- with(step1.results, {
+    ##   data.table(problemStart=min(problemStart),
+    ##              problemEnd=max(problemEnd))
+    ## })
+    ## all.counts.list <- readBigWigSamples(big.problem)
+    ## for(sample.id in names(all.counts.list)){
+    ##   all.counts.list[[sample.id]]$sample.id <- sample.id
+    ## }
+    ## all.counts <- data.table(do.call(rbind, all.counts.list))
+    ## ggplot()+
+    ##   geom_rect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+    ##                 ymin=0, ymax=count),
+    ##             data=all.counts,
+    ##             color="grey")+
+    ##   geom_segment(aes(chromStart/1e3, 0,
+    ##                    xend=chromEnd/1e3, yend=0),
+    ##                data=pred.peaks,
+    ##                color="deepskyblue",
+    ##                size=2)+
+    ##   theme_bw()+
+    ##   theme(panel.margin=grid::unit(0, "cm"))+
+    ##   facet_grid(sample.id ~ ., scales="free")
+
+    sample.id.vec <- sort(unique(paste(pred.peaks$sample.id)))
+    pred.peaks$peak.name <- with(pred.peaks, {
+      sprintf("%s:%d-%d", chrom, chromStart, chromEnd)
+    })
+    peak.name.vec <- sort(unique(pred.peaks$peak.name))
+    peak.mat <- 
+      matrix(0, length(sample.id.vec), length(peak.name.vec),
+             dimnames=list(sample.id=sample.id.vec,
+               peak=peak.name.vec))
+    i.mat <- with(pred.peaks, cbind(paste(sample.id), peak.name))
+    peak.mat[i.mat] <- 1
+    ## d.mat <- dist(peak.mat, method="manhattan")
+    ## fit <- hclust(d.mat, method="average")
+    ##plot(fit)
+
+    ## combine chroms and save bed files later.
+    ##peaks.by.sample <- split(pred.peaks, pred.peaks$sample.id)
+
+    pred.dir <- file.path(data.dir, "PeakSegJoint-predictions")
+    dir.create(pred.dir, showWarnings=FALSE)
+
+    chrom.RData <- file.path(pred.dir, paste0(chrom, ".RData"))
+
+    cat("Saved", ncol(peak.mat), "unique peaks across",
+        nrow(peak.mat), "samples to", chrom.RData, "\n")
+
+    save(pred.peaks, peak.mat, file=chrom.RData)
   }
 }
-step2.peak.list <-
-  mclapply.or.stop(seq_along(step2.problems$problem.name), SegmentStep2)
-
-pred.peaks <- do.call(rbind, step2.peak.list)
-
-## big.problem <- with(step1.results, {
-##   data.table(problemStart=min(problemStart),
-##              problemEnd=max(problemEnd))
-## })
-## all.counts.list <- readBigWigSamples(big.problem)
-## for(sample.id in names(all.counts.list)){
-##   all.counts.list[[sample.id]]$sample.id <- sample.id
-## }
-## all.counts <- data.table(do.call(rbind, all.counts.list))
-## ggplot()+
-##   geom_rect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
-##                 ymin=0, ymax=count),
-##             data=all.counts,
-##             color="grey")+
-##   geom_segment(aes(chromStart/1e3, 0,
-##                    xend=chromEnd/1e3, yend=0),
-##                data=pred.peaks,
-##                color="deepskyblue",
-##                size=2)+
-##   theme_bw()+
-##   theme(panel.margin=grid::unit(0, "cm"))+
-##   facet_grid(sample.id ~ ., scales="free")
-
-sample.id.vec <- sort(unique(paste(pred.peaks$sample.id)))
-pred.peaks$peak.name <- with(pred.peaks, {
-  sprintf("%s:%d-%d", chrom, chromStart, chromEnd)
-})
-peak.name.vec <- sort(unique(pred.peaks$peak.name))
-peak.mat <- 
-  matrix(0, length(sample.id.vec), length(peak.name.vec),
-         dimnames=list(sample.id=sample.id.vec,
-           peak=peak.name.vec))
-i.mat <- with(pred.peaks, cbind(paste(sample.id), peak.name))
-peak.mat[i.mat] <- 1
-## d.mat <- dist(peak.mat, method="manhattan")
-## fit <- hclust(d.mat, method="average")
-##plot(fit)
-
-## combine chroms and save bed files later.
-##peaks.by.sample <- split(pred.peaks, pred.peaks$sample.id)
-
-pred.dir <- file.path(data.dir, "PeakSegJoint-predictions")
-dir.create(pred.dir, showWarnings=FALSE)
-
-chrom.RData <- file.path(pred.dir, paste0(chrom, ".RData"))
-
-save(pred.peaks, peak.mat, file=chrom.RData)
