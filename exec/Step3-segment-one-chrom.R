4646d8aa17b5a815f1ac17796847f7d0fd6b78de
Toby Dylan Hocking <tdhock5@gmail.com>
2015-07-20 15:28:27 -0400

    save chrom in pred.peaks

--- exec/Step3-segment-one-chrom.R
+++ exec/Step3-segment-one-chrom.R
@@ -96,6 +96,7 @@ if(all(sapply(step1.results.list, is.null))){
     mclapply.or.stop(seq_along(step2.problems$problem.name), SegmentStep2)
 
   pred.peaks <- do.call(rbind, step2.peak.list)
+  pred.peaks$chrom <- chrom
 
   if(0 == nrow(pred.peaks)){
     warning("no predicted peaks")
