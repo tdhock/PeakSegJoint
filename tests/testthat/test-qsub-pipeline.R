69baf49ee19e7bb5b33855bec478e00392b0a56f
Toby Dylan Hocking <tdhock5@gmail.com>
2015-08-04 16:56:46 -0400

    check error for overlapping chunks in different files

--- tests/testthat/test-qsub-pipeline.R
+++ tests/testthat/test-qsub-pipeline.R
@@ -1,9 +1,23 @@
+library(testthat)
 context("qsub-pipeline")
 
 exampleData <-
   system.file("exampleData",
               mustWork=TRUE,
               package="PeakSegJoint")
+labels.txt <- file.path(exampleData, "manually_annotated_region_labels.txt")
+overlapping.txt <- file.path(exampleData, "overlapping_labels.txt")
+Step0 <-
+  system.file("exec", "Step0-convert-labels.R",
+              mustWork=TRUE,
+              package="PeakSegJoint")
+cmd.args <- paste(Step0, labels.txt, overlapping.txt)
+
+test_that("overlapping chunks in different files is an error", {
+  out.lines <- system2("Rscript", cmd.args, stderr=TRUE, stdout=TRUE)
+  out.txt <- paste(out.lines, collapse="\n")
+  expect_match(out.txt, "chunks in different label files should not overlap")
+})
 
 pred.RData <- file.path(exampleData, "PeakSegJoint.predictions.RData")
 unlink(pred.RData)
@@ -12,7 +26,6 @@ AllSteps <-
   system.file("exec", "00_AllSteps_qsub.R",
               mustWork=TRUE,
               package="PeakSegJoint")
-labels.txt <- file.path(exampleData, "manually_annotated_region_labels.txt")
 cmd <- paste("QSUB='echo INTERACTIVE && bash' Rscript", AllSteps, labels.txt)
 system(cmd)
 
