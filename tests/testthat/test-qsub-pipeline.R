6e6146175b2e81489aa75628d4f0f69bf59eb0a3
Toby Dylan Hocking <tdhock5@gmail.com>
2015-09-21 16:37:19 -0400

    limit code outside test_that

--- tests/testthat/test-qsub-pipeline.R
+++ tests/testthat/test-qsub-pipeline.R
@@ -18,81 +18,67 @@ exampleDir <- function(){
   file.path(tdir, "exampleData")
 }
 
-three.chunks <- exampleDir()
-labels.txt <- file.path(three.chunks, "manually_annotated_region_labels.txt")
-overlapping.txt <- file.path(three.chunks, "overlapping_labels.txt")
-
-Step0 <-
-  system.file("exec", "Step0-convert-labels.R",
-              mustWork=TRUE,
-              package="PeakSegJoint")
-cmd.args <- paste(Step0, labels.txt, overlapping.txt)
-out.lines <- system2("Rscript", cmd.args, stderr=TRUE, stdout=TRUE)
-
 test_that("overlapping chunks in different files is an error", {
+  overlapping.chunks <- exampleDir()
+  labels.txt <-
+    file.path(overlapping.chunks, "manually_annotated_region_labels.txt")
+  overlapping.txt <- file.path(overlapping.chunks, "overlapping_labels.txt")
+  Step0 <-
+    system.file("exec", "Step0-convert-labels.R",
+                mustWork=TRUE,
+                package="PeakSegJoint")
+  cmd.args <- paste(Step0, labels.txt, overlapping.txt)
+  out.lines <- system2("Rscript", cmd.args, stderr=TRUE, stdout=TRUE)
   out.txt <- paste(out.lines, collapse="\n")
   expect_match(out.txt, "chunks in different label files should not overlap")
 })
 
-cmd <- paste(Rscript, AllSteps, labels.txt)
-system(cmd)
-
 test_that("pipeline trained on 4 samples predicts for 8 samples", {
+  three.chunks <- exampleDir()
+  labels.txt <-
+    file.path(three.chunks, "manually_annotated_region_labels.txt")
+  cmd <- paste(Rscript, AllSteps, labels.txt)
+  system(cmd)
   pred.RData <- file.path(three.chunks, "PeakSegJoint.predictions.RData")
   load(pred.RData)
   expect_equal(nrow(all.peaks.mat), 8)
   starts <- unique(all.peaks.df$chromStart)
   expect_equal(length(starts), ncol(all.peaks.mat))
-})
-
-test.errors.dir <- 
-  file.path(three.chunks, "PeakSegJoint-chunks", "figure-test-errors")
-test.errors.files <- dir(test.errors.dir)
-
-test_that("pipeline trained on 3 chunks generates 3 test error plots", {
+  test.errors.dir <- 
+    file.path(three.chunks, "PeakSegJoint-chunks", "figure-test-errors")
+  test.errors.files <- dir(test.errors.dir)
+  ## pipeline trained on 3 chunks generates 3 test error plots:
   expected.files <- paste0(1:3, ".png")
   expect_true(all(expected.files %in% test.errors.files))
-})
-
-test_that("pipeline trained on 3 chunks generates test error summary", {
+  ## pipeline trained on 3 chunks generates test error summary:
   expect_true("figure-test-error-decreases.png" %in% test.errors.files)
-})
-
-test_that("pipeline trained on 1 file does 3 fold CV", {
+  ## pipeline trained on 1 file does 3 fold CV:
   index.html <- file.path(test.errors.dir, "index.html")
   index.lines <- readLines(index.html)
   cv.line <- grep("cross-validation", index.lines, value=TRUE)
   expect_match(cv.line, "3 fold")
 })
 
-six.chunks <- exampleDir()
-labels.txt <- file.path(six.chunks, "manually_annotated_region_labels.txt")
-other.txt <- file.path(six.chunks, "other_labels.txt")
-cmd <- paste(Rscript, AllSteps, labels.txt, other.txt)
-system(cmd)
-
 test_that("pipeline trained on 8 samples predicts for 8 samples", {
+  six.chunks <- exampleDir()
+  labels.txt <- file.path(six.chunks, "manually_annotated_region_labels.txt")
+  other.txt <- file.path(six.chunks, "other_labels.txt")
+  cmd <- paste(Rscript, AllSteps, labels.txt, other.txt)
+  system(cmd)
   pred.RData <- file.path(six.chunks, "PeakSegJoint.predictions.RData")
   load(pred.RData)
   expect_equal(nrow(all.peaks.mat), 8)
   starts <- unique(all.peaks.df$chromStart)
   expect_equal(length(starts), ncol(all.peaks.mat))
-})
-
-test.errors.dir <- 
-  file.path(six.chunks, "PeakSegJoint-chunks", "figure-test-errors")
-test.errors.files <- dir(test.errors.dir)
-
-test_that("pipeline trained on 6 chunks generates 6 test error plots", {
+  test.errors.dir <- 
+    file.path(six.chunks, "PeakSegJoint-chunks", "figure-test-errors")
+  test.errors.files <- dir(test.errors.dir)
+  ## pipeline trained on 6 chunks generates 6 test error plots:
   expected.files <- paste0(1:6, ".png")
   expect_true(all(expected.files %in% test.errors.files))
-})
-
-test_that("pipeline trained on 6 chunks generates test error summary", {
+  ## pipeline trained on 6 chunks generates test error summary:
   expect_true("figure-test-error-decreases.png" %in% test.errors.files)
-})
-
-test_that("pipeline trained on 2 files does 2 fold CV", {
+  ## pipeline trained on 2 files does 2 fold CV: 
   index.html <- file.path(test.errors.dir, "index.html")
   index.lines <- readLines(index.html)
   cv.line <- grep("cross-validation", index.lines, value=TRUE)
