c7a3ffb0b0903d7ed60512024e14543097554162
Toby Dylan Hocking <tdhock5@gmail.com>
2016-03-02 09:38:09 -0500

    add cDPA code

--- exec/Step1-segment-one-labeled-chunk.R
+++ exec/Step1-segment-one-labeled-chunk.R
@@ -26,6 +26,7 @@ if(length(argv) != 1){
 }
 
 chunk.dir <- argv[1]
+bins.per.problem <- 500L
 chunk.id <- basename(chunk.dir)
 chunks.dir <- dirname(chunk.dir)
 data.dir <- dirname(chunks.dir)
@@ -37,6 +38,7 @@ objs <- load(regions.RData)
 regions$region.i <- 1:nrow(regions)
 chrom <- paste(regions$chrom[1])
 regions[, chromStart1 := chromStart + 1L]
+regions[, id.group := paste(sample.id, sample.group)]
 
 counts.by.sample <- list()
 for(bigwig.file in bigwig.file.vec){
@@ -51,18 +53,110 @@ counts <- do.call(rbind, counts.by.sample)
 counts[, chromStart1 := chromStart + 1L]
 setkey(counts, chromStart1, chromEnd)
 
+ann.colors <-
+  c(noPeaks="#f6f4bf",
+    peakStart="#ffafaf",
+    peakEnd="#ff4c4c",
+    peaks="#a445ee")
 step1.problems.dt <- do.call(rbind, problems.by.res)
+
+ggplot()+
+  ggtitle(regions.RData)+
+  geom_tallrect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3, fill=annotation),
+                color="grey",
+                alpha=0.5,
+                data=regions)+
+  geom_segment(aes(problemStart/1e3, seq_along(bases.per.problem),
+                   xend=problemEnd/1e3, yend=seq_along(bases.per.problem)),
+               data=data.table(step1.problems.dt,
+                               sample.group="problems", sample.id="problems"))+
+  scale_fill_manual(values=ann.colors)+
+  geom_rect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+                ymin=0, ymax=count),
+            fill="grey50",
+            data=counts)+
+  theme_bw()+
+  theme(panel.margin=grid::unit(0, "cm"))+
+  facet_grid(sample.group + sample.id ~ ., scales="free")
+
 Step1Problem <- function(problem.i){
   problem <- step1.problems.dt[problem.i, ]
+  bases.per.bin <- as.integer(problem$bases.per.problem/bins.per.problem)
   problem.name <- paste(problem$problem.name)
-  problem.counts <-
-    counts[! (chromEnd < problem$problemStart |
-                problem$problemEnd < chromStart), ]
-  peak.only <- peak.or.null(problem.counts)
-  if(!is.null(peak.only)){
-    data.table(problem, peak.only)
-  }
-}
+  problem.regions <-
+    regions[! (chromEnd < problem$problemStart |
+                 problem$problemEnd < chromStart), ]
+  setkey(problem.regions, id.group)
+  
+  ggplot()+
+    ggtitle(problem.name)+
+    geom_tallrect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3, fill=annotation),
+                  color="grey",
+                  alpha=0.5,
+                  data=regions)+
+    geom_segment(aes(problemStart/1e3, 0,
+                     xend=problemEnd/1e3, yend=0),
+                 data=problem)+
+    scale_fill_manual(values=ann.colors)+
+    geom_rect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+                  ymin=0, ymax=count),
+              fill="grey50",
+              data=counts)+
+    theme_bw()+
+    theme(panel.margin=grid::unit(0, "cm"))+
+    facet_grid(sample.group + sample.id ~ ., scales="free")
+
+  for(id.group in names(counts.by.sample)){
+    sample.regions <- problem.regions[id.group]
+    if(nrow(sample.regions)){
+      sample.counts <- counts.by.sample[[id.group]]
+      problem.counts <-
+        sample.counts[! (chromEnd < problem$problemStart |
+                           problem$problemEnd < chromStart), ]
+      start <- as.integer(problem$problemStart)
+      model <- segmentBins(
+        problem.counts, start, bases.per.bin, bins.per.problem)
+
+      ggplot()+
+        ggtitle(problem.name)+
+        geom_tallrect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+                          fill=annotation),
+                      color="grey",
+                      alpha=0.5,
+                      data=sample.regions)+
+        geom_segment(aes(problemStart/1e3, -1,
+                         xend=problemEnd/1e3, yend=-1),
+                     size=2,
+                     data=problem)+
+        scale_fill_manual(values=ann.colors)+
+        geom_rect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+                      ymin=0, ymax=count),
+                  fill="grey50",
+                  data=problem.counts)+
+        geom_line(aes((chromStart+chromEnd)/2e3, mean),
+                  data=model$bins)+
+        theme_bw()+
+        theme(panel.margin=grid::unit(0, "cm"))+
+        facet_grid(sample.group + sample.id ~ ., scales="free")
+      
+      model$fit$error$incorrect.regions <- NA
+      for(peaks.str in names(model$fit$peaks)){
+        peaks <- model$fit$peaks[[peaks.str]]
+        error.regions <- PeakErrorChrom(peaks, sample.regions)
+        model$fit$error[peaks.str, "incorrect.regions"] <-
+          with(error.regions, sum(fp+fn))
+      }
+      model$modelSelection$incorrect.regions <-
+        model$fit$error[paste(model$modelSelection$peaks), "incorrect.regions"]
+      target.indices <- with(model$modelSelection, largestContinuousMinimum(
+        incorrect.regions, max.log.lambda-min.log.lambda))
+      model$target <- with(model$modelSelection, c(
+        min.log.lambda[target.indices$start],
+        max.log.lambda[target.indices$end]))
+    }#if(nrow(sample.regions))
+  }#id.group
+  stop("TODO return something")
+}#problem.i
 step1.results.list <-
   mclapply.or.stop(seq_along(step1.problems.dt$problem.name), Step1Problem)
 if(all(sapply(step1.results.list, is.null))){
