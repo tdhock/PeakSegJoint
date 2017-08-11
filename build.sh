a8f8bb4eac7ee4d7b43d862f8fb5ea8768a7fca7
Toby Dylan Hocking <tdhock5@gmail.com>
2017-08-11 09:02:16 -0400

    correct test file path

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ cp -r PeakSegJoint PeakSegJoint-release
 # 6:     test-functions-no-zeros.R  38.127
 # 7: test-functions-PeakSegJoint.R  56.224
 # 8:    test-functions-real-data.R 674.881
-rm PeakSegJoint-release/test-functions-real-data.R
+rm PeakSegJoint-release/tests/testthat/test-functions-real-data.R
 PKG_TGZ=$(R CMD build PeakSegJoint-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
 R CMD INSTALL $PKG_TGZ
 R CMD check --as-cran $PKG_TGZ
